c1ec585cb9b077e20f3a0d3564feaa49b2dc84ef
UmbertoB <umbertobarrosf@gmail.com>
2023-02-17 11:53:50 -0300

    test local database

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-/** @type {import('next').NextConfig} */
-const runtimeCaching = require('next-pwa/cache.js');
-
+const runtimeCaching = require('next-pwa/cache');
 const withPWA = require('next-pwa')({
+  disable: process.env.NODE_ENV === 'development',
   dest: 'public',
-  disableDevLogs: true,
+  register: true,
+  skipWaiting: false,
   runtimeCaching
 });
+const nextConfig = withPWA({});
 
-module.exports = withPWA({
-  // config
-});
+module.exports = nextConfig;
