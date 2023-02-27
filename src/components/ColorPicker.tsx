4732718163fb798761236a64611ae97404a03c98
UmbertoB <umbertobarrosf@gmail.com>
2023-02-27 01:38:22 -0300

    color selected border

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -23,23 +23,23 @@ export const ColorPicker: FC<ColorPickerProps> = ({ color, setColor }) => {
       <div className="flex gap-5">
         <div
           onClick={changeColor(Colors.PALETTE_ONE)}
-          className={`${color === Colors.PALETTE_ONE ? 'border-black' : ''}
+          className={`${color === Colors.PALETTE_ONE ? 'border-gray-900' : ''}
         relative cursor-pointer transition-all border-palette_one border-4 bg-palette_one p-4 rounded-full items-center`}></div>
         <div
           onClick={changeColor(Colors.PALETTE_TWO)}
-          className={`${color === Colors.PALETTE_TWO ? 'border-black' : ''}
+          className={`${color === Colors.PALETTE_TWO ? 'border-gray-900' : ''}
         cursor-pointer transition-all border-palette_two border-4 bg-palette_two p-4 rounded-full items-center`}></div>
         <div
           onClick={changeColor(Colors.PRIMARY)}
-          className={`${color === Colors.PRIMARY ? 'border-black' : ''}
+          className={`${color === Colors.PRIMARY ? 'border-gray-900' : ''}
         cursor-pointer transition-all border-primary border-4 bg-primary p-4 rounded-full items-center`}></div>
         <div
           onClick={changeColor(Colors.PALETTE_THREE)}
-          className={`${color === Colors.PALETTE_THREE ? 'border-black' : ''}
+          className={`${color === Colors.PALETTE_THREE ? 'border-gray-900' : ''}
         cursor-pointer transition-all border-palette_three border-4 bg-palette_three p-4 rounded-full items-center`}></div>
         <div
           onClick={changeColor(Colors.PALETTE_FOUR)}
-          className={`${color === Colors.PALETTE_FOUR ? 'border-black' : ''}
+          className={`${color === Colors.PALETTE_FOUR ? 'border-gray-900' : ''}
         cursor-pointer transition-all border-palette_four border-4 bg-palette_four p-4 rounded-full items-center`}></div>
       </div>
     </div>
