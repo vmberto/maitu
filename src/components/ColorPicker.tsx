d90010a3fee05b78d79a8a5685a4be1399ef4022
UmbertoB <umbertobarrosf@gmail.com>
2023-03-07 20:51:56 +0100

    Color picker fix

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -8,21 +8,21 @@ export const Colors = [
     'palette_four',
 ];
 
-const Backgrounds = {
-    primary: 'bg-primary',
-    palette_one: 'bg-palette_one',
-    palette_two: 'bg-palette_two',
-    palette_three: 'bg-palette_three',
-    palette_four: 'bg-palette_four',
-}
+const Backgrounds = Map<string, string>([
+   [ 'primary', 'bg-primary'],
+    ['palette_one', 'bg-palette_one'],
+    ['palette_two', 'bg-palette_two'],
+    ['palette_three', 'bg-palette_three'],
+   [ 'palette_four', 'bg-palette_four'],
+ ]);
 
-const Borders = {
-    primary: 'border-primary',
-    palette_one: 'border-palette_one',
-    palette_two: 'border-palette_two',
-    palette_three: 'border-palette_three',
-    palette_four: 'border-palette_four',
-}
+const Borders = Map<string, string>([
+    [ 'primary', 'border-primary'],
+    ['palette_one', 'border-palette_one'],
+    ['palette_two', 'border-palette_two'],
+    ['palette_three', 'border-palette_three'],
+    [ 'palette_four', 'border-palette_four'],
+]);
 
 interface ColorPickerProps {
   color: string;
@@ -40,8 +40,8 @@ export const ColorPicker: FC<ColorPickerProps> = ({ color, setColor }) => {
       <div className="flex gap-5">
         {Colors.map(c => <div
               onClick={changeColor(c)}
-              className={`${color === c ? 'border-gray-900' : Borders[c]}
-        relative cursor-pointer transition-all border-4 ${Backgrounds[c]} p-4 rounded-full items-center`}></div>)}
+              className={`${color === c ? 'border-gray-900' : Borders.get(c)}
+        relative cursor-pointer transition-all border-4 ${Backgrounds.get(c)} p-4 rounded-full items-center`}></div>)}
       </div>
     </div>
     );
