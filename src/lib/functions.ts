22ba4ed15ab3a8bf6969d42579d4dd2292ad4a5a
UmbertoB <umbertobarrosf@gmail.com>
2023-12-29 20:10:50 -0300

    Refactor todos view

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,3 +1,5 @@
+import {GenericEvent} from "../../types/events";
+
 export const formatDate = (date: Date) => {
     const day = date.getDate();
     const month = date.getMonth() + 1;
@@ -8,3 +10,5 @@ export const formatDate = (date: Date) => {
         .toString()
         .padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
 };
+
+export const stopPropagationFn = (e: GenericEvent) => e.stopPropagation();
--- src/ui/view/TodosView/index.tsx
+++ src/ui/view/TodosView/index.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import {useTodos} from 'src/hooks/useTodos';
 import {FontColor, HexColors} from 'src/lib/colors';
 import {GenericEvent} from "../../../../types/events";
+import {stopPropagationFn} from "src/lib/functions";
 
 const TodosView: FC = () => {
     const newTodoInputRef = useRef({} as HTMLTextAreaElement);
@@ -27,66 +28,63 @@ const TodosView: FC = () => {
     } = useTodos(newTodoInputRef.current);
 
     return (
-        <div className="relative">
+        <div className="relative" onClick={handleClickScreen}>
             {clickScreenFocusHandler && (
                 <div className="absolute w-full h-full" onClick={removeFocus}></div>
             )}
-            <div className="min-h-screen" onClick={handleClickScreen}>
-                <div className="max-w-xl my-0 mx-auto">
-                    <div className="flex items-center py-2">
-                        <Link className="pl-5 flex h-12" href=".."
-                              onClick={(e: GenericEvent) => e.stopPropagation()}>
-                            <ArrowLeftIcon
-                                className="relative cursor-pointer w-6 mr-5"
-                                color={HexColors.get(selectedTodoList.color)}
-                            />
-                        </Link>
-                        <h1 className={`text-2xl font-bold pr-5 ${FontColor.get(selectedTodoList.color)}`}>
-                            {selectedTodoList?.title}
-                        </h1>
-                    </div>
-                    <div id="todos" className="mb-60 px-5 pb-5">
-                        {todosToComplete.map((t) => (
-                            <TodoInput
-                                key={t.id}
-                                id={t.id}
-                                value={t.title}
-                                todoData={t}
-                                handleCompleteTodo={handleCompleteTodo}
-                                onClick={(e) => e.stopPropagation()}
-                                onKeyDown={handleKeyPress}
-                                onBlur={updateTodo(t)}
-                                onFocus={handleInputFocus(t)}
-                                onChange={handleChangeExistingTodo(t)}
-                            />
-                        ))}
-                        <TodoInput
-                            id="new-todo"
-                            ref={newTodoInputRef}
-                            value={newTodo.title}
-                            onChange={handleChangeNewTodo}
-                            onFocus={handleInputFocus(newTodo)}
-                            onBlur={addTodo}
-                            onKeyDown={handleKeyPressAdd}
+            <div className="max-w-xl my-0 mx-auto">
+                <div className="flex items-center py-2">
+                    <Link className="pl-5 flex h-12" onClick={stopPropagationFn} href="..">
+                        <ArrowLeftIcon
+                            className="relative cursor-pointer w-6 mr-3"
+                            color={HexColors.get(selectedTodoList.color)}
                         />
-                        <h1 className="text-sm border-t-2 pt-5 text-center font-light text-gray-500">
-                            Click to add new Todo
-                        </h1>
-                    </div>
-                    {!!completedTodos.length && (
-                        <div id="complete-todos" className="px-5 pb-5">
-                            <div className="flex align-middle text-lg font-semibold mt-5">
-                                <h2>Complete Todos</h2>
-                                <span className="text-sm font-semibold ml-auto">{completedTodos.length}</span>
-                            </div>
-                            <div id="Todos">
-                                {completedTodos.map((t) => (
-                                    <TodoInput key={t.id} id={t.id} todoData={t} value={t.title} disabled/>
-                                ))}
-                            </div>
-                        </div>
-                    )}
+                    </Link>
+                    <h1 className={`text-2xl font-bold pr-5 ${FontColor.get(selectedTodoList.color)}`}>
+                        {selectedTodoList?.title}
+                    </h1>
                 </div>
+                <div id="todos" className="mb-60 px-5 pb-5">
+                    {todosToComplete.map((t) => (
+                        <TodoInput
+                            key={t.id}
+                            id={t.id}
+                            value={t.title}
+                            todoData={t}
+                            handleCompleteTodo={handleCompleteTodo}
+                            onClick={(e) => e.stopPropagation()}
+                            onKeyDown={handleKeyPress}
+                            onBlur={updateTodo(t)}
+                            onFocus={handleInputFocus(t)}
+                            onChange={handleChangeExistingTodo(t)}
+                        />
+                    ))}
+                    <TodoInput
+                        id="new-todo"
+                        ref={newTodoInputRef}
+                        value={newTodo.title}
+                        onChange={handleChangeNewTodo}
+                        onFocus={handleInputFocus(newTodo)}
+                        onBlur={addTodo}
+                        onKeyDown={handleKeyPressAdd}
+                    />
+                    <h1 className="text-sm border-t-2 pt-5 text-center font-light text-gray-500">
+                        Click to add new Todo
+                    </h1>
+                </div>
+                {!!completedTodos.length && (
+                    <div id="complete-todos" className="px-5 pb-5">
+                        <div className="flex align-middle text-lg font-semibold mt-5">
+                            <h2>Complete Todos</h2>
+                            <span className="text-sm font-semibold ml-auto">{completedTodos.length}</span>
+                        </div>
+                        <div id="Todos">
+                            {completedTodos.map((t) => (
+                                <TodoInput key={t.id} id={t.id} todoData={t} value={t.title} disabled/>
+                            ))}
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
