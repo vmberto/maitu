23897a91277e5af60e535e43934bbe9ddb105956
UmbertoB <umbertobarrosf@gmail.com>
2023-12-30 00:42:06 -0300

    Inter font from next/font package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,23 @@
 import Head from 'next/head';
 import 'src/styles/globals.css';
-import { AppProps } from 'next/app';
+import {AppProps} from 'next/app';
+import {Inter} from 'next/font/google';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Maitu</title>
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+const inter = Inter({subsets: ['latin']});
+
+export default function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>Maitu</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+            </Head>
+            <main className={inter.className}>
+                <Component {...pageProps} />
+            </main>
+        </>
+    );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,22 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import {Head, Html, Main, NextScript} from 'next/document';
+
 
 export default function Document() {
-  return (
-    <Html lang="pt-br">
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/touch-icon.png"></link>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-        <meta name="theme-color" content="#317EFB" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="pt-br">
+            <Head>
+                <meta charSet="utf-8"/>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+                <meta name="description" content="Description"/>
+                <meta name="keywords" content="Keywords"/>
+                <link rel="manifest" href="/manifest.json"/>
+                <link rel="apple-touch-icon" href="/touch-icon.png"></link>
+                <meta name="theme-color" content="#317EFB"/>
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    );
 }
