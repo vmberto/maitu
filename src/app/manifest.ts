05338231c5dd392ee6e3bd9205f56709e3b30157
UmbertoB <umbertobarrosf@gmail.com>
2024-01-17 23:53:04 -0300

    manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'maitu',
+    description: 'an application to save your goals',
+    short_name: 'maitu',
+    theme_color: '#ffffff',
+    background_color: '#004740',
+    display: 'fullscreen',
+    orientation: 'portrait',
+    scope: '/',
+    start_url: '/',
+  };
+}
