0912020675549c72f259864e59a52889535569f1
UmbertoB <umbertobarrosf@gmail.com>
2024-02-01 22:34:23 -0300

    remove comments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const withSerwist = require('@serwist/next').default({
-  // Note: This is only an example. If you use Pages Router,
-  // use something else that works, such as "service-worker/index.ts".
   swSrc: 'public/sw.ts',
   swDest: 'public/sw.js',
   disable: process.env.NODE_ENV === 'development',
