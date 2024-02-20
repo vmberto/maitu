c539fa6214302172cfff4413ee219afa2dc0bd0a
UmbertoB <umbertobarrosf@gmail.com>
2024-02-20 18:40:50 -0300

    Fix lint

--- src/app/lists/components/Lists.tsx
+++ src/app/lists/components/Lists.tsx
@@ -8,12 +8,10 @@ import { ListDetailSlideOver } from '@/src/app/lists/components/ListDetailSlideO
 import { NewListButton } from '@/src/app/lists/components/NewListButton';
 import { useLists } from '@/src/app/lists/provider';
 import { DragAndDropWrapper } from '@/src/components/dnd/DragAndDropWrapper';
-import { useModals } from '@/src/providers/slideover.provider';
 
 export const Lists = () => {
   const [open, setOpen] = useState(false);
   const { updateListsOrder, handleAddList, lists } = useLists();
-  const { modalData } = useModals();
 
   return (
     <div className="mx-auto mb-60 mt-0 max-w-xl p-5">
