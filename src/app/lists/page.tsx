df5315bea077b49731d74fc9176f2f3a0cbb546c
UmbertoB <umbertobarrosf@gmail.com>
2024-02-20 00:31:06 -0300

    loading state only for lists

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,8 +1,8 @@
 import { Suspense } from 'react';
 
 import { ListsContainer } from '@/src/app/lists/components/ListsContainer';
+import ListsLoading from '@/src/app/lists/components/Loading/ListsLoading';
 import { MaituHeader } from '@/src/app/lists/components/MaituHeader';
-import ListsLoading from '@/src/app/lists/loading';
 
 export default async function ListsPage() {
   return (
