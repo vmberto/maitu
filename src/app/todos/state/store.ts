a441a7bf1fcd43134e3b2241e461d39e9914f9bc
UmbertoB <umbertobarrosf@gmail.com>
2024-05-22 22:17:22 -0300

    simplifying

--- src/app/todos/state/store.ts
+++ src/app/todos/state/store.ts
@@ -1,13 +1,6 @@
 import { create } from 'zustand';
 
-import {
-  handleAddTodo,
-  handleChangeExistingTodo,
-  handleChangeNewTodo,
-  handleCompleteTodo,
-  handleRemoveOrUpdateTitle,
-  handleUpdateTodo,
-} from '@/src/app/todos/state/actions';
+import { add, remove, update } from '@/src/actions/todos.service';
 import type { TextareaChangeEventHandler } from '@/types/events';
 import type { List, Todo } from '@/types/main';
 
@@ -29,28 +22,106 @@ export interface TodosState {
   handleCompleteTodo: (todo: Todo, executionTimeout: any) => void;
 }
 
-export const useTodosStore = create<TodosState>()((set, get) => ({
-  todos: [] as Todo[],
-  currentTodo: {} as Partial<Todo>,
-  newTodo: {} as Partial<Todo>,
+export const useTodosStore = create<TodosState>((set: any, get: any) => ({
+  todos: [],
+  currentTodo: {},
+  newTodo: {},
   selectedList: {} as List,
 
-  setList: (selectedList: List) => set(() => ({ selectedList })),
-  setTodos: (todos: Todo[]) => set(() => ({ todos })),
-  handleInputFocus: (currentTodo) => set(() => ({ currentTodo })),
+  setTodos: (todos: Todo[]) => set({ todos }),
+  setList: (selectedList: List) => set({ selectedList }),
+  handleInputFocus: (currentTodo: Partial<Todo>) => set({ currentTodo }),
 
-  handleChangeNewTodo: (e: TextareaChangeEventHandler) =>
-    handleChangeNewTodo(e, get, set),
-  handleAddTodo: () => handleAddTodo(get, set),
+  handleAddTodo: async () => {
+    const { newTodo, selectedList } = get();
+    const { title } = newTodo;
 
-  handleUpdateTodo: (todo: Todo) => handleUpdateTodo(todo, get, set),
+    if (title && title.length > 0) {
+      const todo = {
+        complete: false,
+        description: '',
+        createdAt: new Date().toISOString(),
+        listId: selectedList._id,
+        title,
+      } as Todo;
 
-  handleChangeExistingTodo: (e: TextareaChangeEventHandler) =>
-    handleChangeExistingTodo(e, get, set),
+      set((state: TodosState) => ({
+        newTodo: { title: '' },
+        todos: [...state.todos, todo],
+      }));
 
-  handleRemoveOrUpdateTitle: (todo: Todo) =>
-    handleRemoveOrUpdateTitle(todo, get, set),
+      const response = await add(todo);
+      set((state: TodosState) => ({
+        todos: state.todos.map((t: Todo) =>
+          t.createdAt === todo.createdAt ? response : t,
+        ),
+      }));
+    }
+  },
 
-  handleCompleteTodo: (todo: Todo, executionTimeout: any) =>
-    handleCompleteTodo(todo, executionTimeout, get, set),
+  handleCompleteTodo: async (t: Todo) => {
+    if (!t._id) return;
+
+    const todoId = t._id.toString();
+
+    set((state: TodosState) => {
+      return {
+        todos: state.todos.map((todo) =>
+          todo._id === todoId
+            ? { ...todo, complete: true, completedAt: new Date().toISOString() }
+            : todo,
+        ),
+      };
+    });
+  },
+
+  handleChangeExistingTodo: (e: TextareaChangeEventHandler) => {
+    const { currentTodo } = get();
+    const { value } = e.target;
+
+    set((state: TodosState) => ({
+      todos: state.todos.map((t: Todo) =>
+        t._id === currentTodo._id ? { ...t, title: value } : t,
+      ),
+    }));
+  },
+
+  handleRemoveOrUpdateTitle: async (t: Todo) => {
+    if (t.title.length <= 0 && t._id) {
+      set((state: TodosState) => ({
+        todos: state.todos.filter((todo: Todo) => todo._id !== t._id),
+      }));
+      await remove(t._id.toString());
+      return;
+    }
+
+    const { currentTodo } = get();
+    if (t.title !== currentTodo.title && t._id) {
+      set((state: TodosState) => ({
+        todos: state.todos.map((todo: Todo) =>
+          todo._id === t._id ? { ...t } : todo,
+        ),
+      }));
+      await update(t._id.toString(), t);
+    }
+  },
+
+  handleUpdateTodo: async (updatedTodo: Todo) => {
+    if (updatedTodo._id) {
+      set((state: TodosState) => ({
+        todos: state.todos.map((todo: Todo) =>
+          todo._id === updatedTodo._id ? { ...updatedTodo } : todo,
+        ),
+      }));
+
+      await update(updatedTodo._id.toString(), updatedTodo);
+    }
+  },
+
+  handleChangeNewTodo: (e: TextareaChangeEventHandler) => {
+    const { value } = e.target;
+    set((state: TodosState) => ({
+      newTodo: { ...state.newTodo, title: value },
+    }));
+  },
 }));
