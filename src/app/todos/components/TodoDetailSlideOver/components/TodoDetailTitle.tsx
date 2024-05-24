1678863abfa2571497a33b38942d8d7e5ec1ad8f
UmbertoB <umbertobarrosf@gmail.com>
2024-05-24 15:53:17 -0300

    fix completed at icon bug

--- src/app/todos/components/TodoDetailSlideOver/components/TodoDetailTitle.tsx
+++ src/app/todos/components/TodoDetailSlideOver/components/TodoDetailTitle.tsx
@@ -10,24 +10,22 @@ type TodoDetailTitleProps = {
   currentTodo?: Todo;
 };
 
-export const TodoDetailTitle = ({ currentTodo }: TodoDetailTitleProps) => {
-  return (
-    <>
-      <PlayCircleIcon
-        className="mb-1 mr-1 inline size-6"
-        color={HexColors.get('primary')}
-      />
+export const TodoDetailTitle = ({ currentTodo }: TodoDetailTitleProps) => (
+  <>
+    <PlayCircleIcon
+      className="mb-1 mr-1 inline size-6"
+      color={HexColors.get('primary')}
+    />
 
-      <div className="inline">{currentTodo?.title}</div>
-      <div className="mt-2 flex flex-row items-center gap-2 text-sm text-gray-500">
-        {formatDate(currentTodo?.createdAt)}
-        {currentTodo?.createdAt && (
-          <>
-            <CheckCircleIcon className="inline size-5" color="#5aee5c" />
-            {formatDate(currentTodo?.completedAt)}
-          </>
-        )}
-      </div>
-    </>
-  );
-};
+    <div className="inline">{currentTodo?.title}</div>
+    <div className="mt-2 flex flex-row items-center gap-2 text-sm text-gray-500">
+      {formatDate(currentTodo?.createdAt)}
+      {currentTodo?.completedAt && (
+        <>
+          <CheckCircleIcon className="inline size-5" color="#5aee5c" />
+          {formatDate(currentTodo?.completedAt)}
+        </>
+      )}
+    </div>
+  </>
+);
